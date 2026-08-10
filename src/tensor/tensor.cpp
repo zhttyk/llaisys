79d0c46514b771e6ec0f426802588d036c6cfac3
zhttyk <1739kyttyk@gmail.com>
2026-08-10 11:03:55 +0800

    feat(tensor): implement host-to-device load

--- src/tensor/tensor.cpp
+++ src/tensor/tensor.cpp
@@ -184,7 +184,13 @@ tensor_t Tensor::slice(size_t dim, size_t start, size_t end) const {
 }
 
 void Tensor::load(const void *src_) {
-    TO_BE_IMPLEMENTED();
+    core::context().setDevice(this->deviceType(), this->deviceId());
+    core::context().runtime().api()->memcpy_sync(
+        this->data(),
+        src_,
+        this->numel() * this->elementSize(),
+        LLAISYS_MEMCPY_H2D);
+    //参考L156-161
 }
 
 tensor_t Tensor::contiguous() const {
